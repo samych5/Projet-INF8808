d824a9ce62ec3fa37522445f43b7d45b3e8c73b9
Lchakib <chakib.lhachemi@polymtl.ca>
2026-04-03 23:25:49 -0400

    ajout des graphes box plot et bar chart dans la page

--- sections.py
+++ sections.py
@@ -9,6 +9,12 @@ from graphs.scatter.template import make_section as make_scatter_section, get_st
 from graphs.jitter.callbacks import register_callbacks as register_jitter_callbacks
 from graphs.jitter.template import make_section as make_jitter_section, get_steps_number as jitter_steps
 
+from graphs.box_plot.callbacks import register_callbacks as register_box_plot_callbacks
+from graphs.box_plot.template import make_section as make_box_plot_steps_section, get_steps_number as box_plot_steps
+
+from graphs.bar_chart.callbacks import register_callbacks as register_bar_chart_callbacks
+from graphs.bar_chart.template import make_section as make_bar_chart_steps_section, get_steps_number as bar_chart_steps
+
 class SectionParameters:
     def __init__(
         self,
@@ -23,6 +29,8 @@ class SectionParameters:
 SECTIONS = [
     SectionParameters(make_scatter_section, scatter_steps, register_scatter_callbacks),
     SectionParameters(make_jitter_section, jitter_steps, register_jitter_callbacks),
+    SectionParameters(make_box_plot_steps_section, box_plot_steps, register_box_plot_callbacks),
+    SectionParameters(make_bar_chart_steps_section, bar_chart_steps, register_bar_chart_callbacks),
 ]
 
 DEFAULT_STEP = 1
